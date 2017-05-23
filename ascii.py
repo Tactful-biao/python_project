1f4651d1e2517b273c74e6f480f5dd873514f396
Shibiao Sun <1208339113@qq.com>
2017-05-23 22:25:21 +0800

    Update ascii.py

--- ascii.py
+++ ascii.py
@@ -1,12 +1,12 @@
 from PIL import Image
-import argparse
+import argparse  # 参考https://blog.ixxoo.me/argparse.html
 
 parse = argparse.ArgumentParser()
-parse.add_argument('file')
-parse.add_argument('-o', '--output')
-parse.add_argument('--width', type=int, default=80)
-parse.add_argument('--height', type=int, default=70)
-
+parse.add_argument('file')   #输入文件
+parse.add_argument('-o', '--output')   # 输出文件
+parse.add_argument('--width', type=int, default=80)  #设置输出的字画符宽度
+parse.add_argument('--height', type=int, default=70)   #设置输出的字画符高度
+# 获得参数
 args = parse.parse_args()
 
 IMG = args.file
@@ -14,8 +14,8 @@ WIDTH = args.width
 HEIGHT = args.height
 OUTPUT = args.output
 
-ascii_char = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ")
-
+ascii_char = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ")   #设置字画符的组成字符
+# 将256灰度映射到70个字符上
 def get_char(r, g, b, alpha = 256):
     if alpha == 0:
         return ' '
@@ -26,8 +26,8 @@ def get_char(r, g, b, alpha = 256):
     return ascii_char[int(gray/unit)]
 
 if __name__ == '__main__':
-    im = Image.open(IMG)
-    im = im.resize((WIDTH, HEIGHT), Image.NEAREST)
+    im = Image.open(IMG)   #打开图片
+    im = im.resize((WIDTH, HEIGHT), Image.NEAREST)   #重新确定图片大小
 
     txt = ''
 
@@ -36,10 +36,10 @@ if __name__ == '__main__':
             txt += get_char(*im.getpixel((j, i)))
         txt += '\n'
     print(txt)
-
-    if OUTPUT:
+    # 将字符串输出到本地文件
+    if OUTPUT:  #如果给出了文件保存地址则保存在给出的地址
         with open(OUTPUT, 'w') as f:
             f.write(txt)
-    else:
+    else:     #否则保存在当前目录output.txt内
         with open("output.txt", 'w') as f:
             f.write(txt)
